fffd050d5103c63c9ad0e7059e507c846e68050e
spike91 <ivanpanas91@gmail.com>
2018-11-08 15:31:53 +0200

    GenreAdapter

--- app/src/main/java/com/example/user/panasivan_android_task2/MainActivity.java
+++ app/src/main/java/com/example/user/panasivan_android_task2/MainActivity.java
@@ -1,18 +1,46 @@
 package com.example.user.panasivan_android_task2;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends ListViewLoader {
+public class MainActivity extends AppCompatActivity {
     JsonHelper helper;
     List<Film> films;
+    private List<Genre> genres = new ArrayList<>();
+
+    ListView genresList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         helper = new JsonHelper(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         films = helper.getFilms();
+
+        for(Film film: films){
+            for(Genre genre: film.getGenres()){
+                if(!genres.contains(genre)){
+                    genres.add(genre);
+                }
+            }
+        }
+
+        genresList = findViewById(R.id.genresList);
+        GenreAdapter genreAdapter = new GenreAdapter(this, R.layout.genre_item, genres);
+        genresList.setAdapter(genreAdapter);
+        AdapterView.OnItemClickListener genreListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Genre selectedGenre = (Genre) adapterView.getItemAtPosition(i);
+            }
+        };
+        genresList.setOnItemClickListener(genreListener);
     }
 
 
